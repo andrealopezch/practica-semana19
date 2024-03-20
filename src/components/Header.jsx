49d15709ad7e1b120c7310d099648696b06bfc0b
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2024-03-20 17:14:03 -0600

    Se agrego contenido para un encabezado sencillo usando bootstrap

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-primary text-white py-4">
+      <div className="container">
+        <h1 className="text-center">¡Bienvenido a la Tienda de Productos!</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
