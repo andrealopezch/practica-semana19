f282054d6695e72a40c3d53ffc6c7df9e35d56ff
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2024-03-20 17:16:04 -0600

    Se importaron los demas componentes al index.js usando la ruta de conexion de React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from "react-dom";
+import Productos from "./components/Productos";
+import productosData from "./data/ProductosData";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <p>Hola mundo!</p>
+    <Header />
+    <Productos productosData={productosData} />
   </>
 );
