e7166b165546d7299b5d1cf542608da451ef53ea
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2024-03-20 17:07:36 -0600

    Se agrego la base de datos con los productos a data

---
+++ src/data/ProductosData.jsx
@@ -0,0 +1,74 @@
+const productosData = [
+  {
+    id: "001",
+    nombreProducto: "Alitas",
+    descripcion: "Orden de Alitas, cerveza y acompañamientos",
+    foto: "https://images.pexels.com/photos/19786206/pexels-photo-19786206/free-photo-of-restaurante-cerveza-pollo-comida-rapida.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 100,
+  },
+  {
+    id: "002",
+    nombreProducto: "Pizza",
+    descripcion: "Pizza de su preferencia, orden individual",
+    foto: "https://images.pexels.com/photos/19786207/pexels-photo-19786207/free-photo-of-pizza-tabla-de-cortar-salchichas-comida-rapida.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 150,
+  },
+  {
+    id: "003",
+    nombreProducto: "Hamburguesa",
+    descripcion: "Hamburguesa acompañada de papas fritas, bebida incluida",
+    foto: "https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 115,
+  },
+  {
+    id: "004",
+    nombreProducto: "Sushi",
+    descripcion: "Sushi de su preferencia, 6 rollos",
+    foto: "https://images.pexels.com/photos/3298596/pexels-photo-3298596.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 130,
+  },
+  {
+    id: "005",
+    nombreProducto: "Ensalada",
+    descripcion: "Ensalada de la casa, incluye bebida y aderezo de su elección",
+    foto: "https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 90,
+  },
+  {
+    id: "006",
+    nombreProducto: "Pasta",
+    descripcion: "Orden de pasta con la salsa de su preferencia",
+    foto: "https://images.pexels.com/photos/12622975/pexels-photo-12622975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 110,
+  },
+  {
+    id: "007",
+    nombreProducto: "Pastel",
+    descripcion: "Rebanada de pastel de chocolate",
+    foto: "https://images.pexels.com/photos/1854652/pexels-photo-1854652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 70,
+  },
+  {
+    id: "008",
+    nombreProducto: "Tacos",
+    descripcion: "Orden de 3 tacos",
+    foto: "https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 100,
+  },
+  {
+    id: "009",
+    nombreProducto: "Elote sticks",
+    descripcion: "Tiras de elote en cono",
+    foto: "https://images.pexels.com/photos/2734288/pexels-photo-2734288.jpeg",
+    precio: 120,
+  },
+  {
+    id: "010",
+    nombreProducto: "HotDog",
+    descripcion: "2 piezas por orden con acompañamiento",
+    foto: "https://images.pexels.com/photos/4518656/pexels-photo-4518656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    precio: 80,
+  },
+];
+
+export default productosData;
